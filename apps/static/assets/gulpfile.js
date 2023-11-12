68c9cb60f0c7fba2279696a8010dda34066e8194
Pyromma <struzik.wroc@gmail.com>
2023-11-12 19:36:15 +0100

    replace

--- apps/static/assets/gulpfile.js
+++ apps/static/assets/gulpfile.js
@@ -1,59 +1,35 @@
-/*
-
-=========================================================
-* AppSeed - Simple SCSS compiler via Gulp
-=========================================================
-
-*/
-
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var cleanCss = require('gulp-clean-css');
 var gulp = require('gulp');
-const npmDist = require('gulp-npm-dist');
-var sass = require('gulp-sass')(require('node-sass'));
-var wait = require('gulp-wait');
+var path = require('path');
+var sass = require('gulp-sass')(require('sass'));
+var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
-var rename = require("gulp-rename");
+var open = require('gulp-open');
 
-// Define COMMON paths
-
-const paths = {
-    src: {
-        base: './',
-        css: './css',
-        scss: './scss',
-        node_modules: './node_modules/',
-        vendor: './vendor'
-    }
+var Paths = {
+  HERE: './',
+  DIST: 'dist/',
+  CSS: './assets/css/',
+  SCSS_TOOLKIT_SOURCES: './assets/scss/soft-ui-dashboard.scss',
+  DARK_MODE_SOURCE: './assets/scss/dark-theme-core.scss',
+  SCSS: './assets/scss/**/**'
 };
 
-// Compile SCSS
-gulp.task('scss', function() {
-    return gulp.src([paths.src.scss + '/material-kit.scss'])
-        .pipe(wait(500))
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['> 1%']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.src.css))
-        .pipe(browserSync.stream());
+gulp.task('compile-scss', function() {
+  return gulp.src([Paths.SCSS_TOOLKIT_SOURCES, Paths.DARK_MODE_SOURCE])
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(gulp.dest(Paths.CSS));
 });
 
-// Minify CSS
-gulp.task('minify:css', function() {
-    return gulp.src([
-            paths.src.css + '/material-kit.css'
-        ])
-        .pipe(cleanCss())
-        .pipe(rename(function(path) {
-            // Updates the object in-place
-            path.extname = ".min.css";
-        }))
-        .pipe(gulp.dest(paths.src.css))
+gulp.task('watch', function() {
+  gulp.watch(Paths.SCSS, gulp.series('compile-scss'));
 });
 
-// Default Task: Compile SCSS and minify the result
-gulp.task('default', gulp.series('scss', 'minify:css'));
+gulp.task('open', function() {
+  gulp.src('pages/dashboard.html')
+    .pipe(open());
+});
+
+gulp.task('open-app', gulp.parallel('open', 'watch'));
