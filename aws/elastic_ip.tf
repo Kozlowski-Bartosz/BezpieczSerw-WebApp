2d553aa6fe1bed837e3cde89bae852a69506c1f9
Pyromma <struzik.wroc@gmail.com>
2023-11-12 19:42:51 +0100

    Revert "Add files via upload"
    This reverts commit 3928a98f8556b6c7e4221fa0b617e98114bf2366.

--- aws/elastic_ip.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "lb" {
-  instance                  = aws_instance.bastion.id
-  associate_with_private_ip = aws_instance.bastion.private_ip
-}
--- aws/keyms.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_kms_key" "key_mngmnt_service" {
-  description = "mey"
-}
